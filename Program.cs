a6180dbb0ab447e516d5bac5f59ee51388fb4ed9
vladpotuk <vladpotuk01@gmail.com>
2024-03-20 19:09:36 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class MainProgram
+{
+    static void Main(string[] args)
+    {
+        Predicate<int> isEven = Operations.IsEven;
+        Predicate<int> isOdd = Operations.IsOdd;
+        Predicate<int> isPrime = Operations.IsPrime;
+        Predicate<int> isFibonacci = Operations.IsFibonacci;
+
+        Console.Write("Введіть число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Число парне: {isEven(num)}");
+        Console.WriteLine($"Число непарне: {isOdd(num)}");
+        Console.WriteLine($"Число просте: {isPrime(num)}");
+        Console.WriteLine($"Число Фібоначчі: {isFibonacci(num)}");
+    }
+}
