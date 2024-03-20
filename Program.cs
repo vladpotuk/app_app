c811217b0efd601d3689e266e7eef5e27137d685
vladpotuk <vladpotuk01@gmail.com>
2024-03-20 18:57:21 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+delegate double ArithmeticOperation(double x, double y);
+
+class Program
+{
+   
+    static double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    
+    static double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+
+    
+    static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    static void Main(string[] args)
+    {
+        
+        ArithmeticOperation add = Add;
+        ArithmeticOperation subtract = Subtract;
+        ArithmeticOperation multiply = Multiply;
+
+        
+        Console.Write("Введіть перше число: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        
+        Console.WriteLine($"Сума: {add(num1, num2)}");
+        Console.WriteLine($"Різниця: {subtract(num1, num2)}");
+        Console.WriteLine($"Добуток: {multiply(num1, num2)}");
+    }
+}
