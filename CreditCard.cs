7220f626b0958f3c3a36b4d662c7bfc1ca395fca
vladpotuk <vladpotuk01@gmail.com>
2024-03-20 20:22:26 +0200

    update

---
+++ CreditCard.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class CreditCard
+{
+    public string CardNumber { get; set; }
+    public string CardHolderName { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public string PIN { get; set; }
+    public decimal CreditLimit { get; set; }
+    public decimal Balance { get; set; }
+
+    
+    public event Action<decimal> AccountReplenished;
+    public event Action<decimal> MoneySpent;
+    public event Action CreditStarted;
+    public event Action<decimal> LimitReached;
+    public event Action<string> PINChanged;
+
+    
+    public CreditCard(string cardNumber, string cardHolderName, DateTime expiryDate, string pin, decimal creditLimit)
+    {
+        CardNumber = cardNumber;
+        CardHolderName = cardHolderName;
+        ExpiryDate = expiryDate;
+        PIN = pin;
+        CreditLimit = creditLimit;
+    }
+
+    
+    public void ReplenishAccount(decimal amount)
+    {
+        Balance += amount;
+        AccountReplenished?.Invoke(amount);
+    }
+
+    public void SpendMoney(decimal amount)
+    {
+        Balance -= amount;
+        MoneySpent?.Invoke(amount);
+    }
+
+    public void StartCredit()
+    {
+        CreditStarted?.Invoke();
+    }
+
+    public void CheckLimit()
+    {
+        if (Balance >= CreditLimit)
+        {
+            LimitReached?.Invoke(Balance);
+        }
+    }
+
+    public void ChangePIN(string newPIN)
+    {
+        PIN = newPIN;
+        PINChanged?.Invoke(newPIN);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CreditCard card = new CreditCard("1234 5678 9012 3456", "John Doe", DateTime.Now.AddYears(3), "1234", 5000);
+
+        
+        card.AccountReplenished += amount => Console.WriteLine($"Рахунок поповнено на {amount} грн");
+        card.MoneySpent += amount => Console.WriteLine($"З рахунку списано {amount} грн");
+        card.CreditStarted += () => Console.WriteLine("Початок використання кредитних коштів");
+        card.LimitReached += balance => Console.WriteLine($"Досягнуто ліміт кредиту: {balance} грн");
+        card.PINChanged += newPIN => Console.WriteLine($"PIN змінено на {newPIN}");
+
+        
+        card.ReplenishAccount(1000);
+        
+        card.SpendMoney(500);
+        
+        card.StartCredit();
+        
+        card.ChangePIN("5678");
+        
+        card.CheckLimit();
+    }
+}
