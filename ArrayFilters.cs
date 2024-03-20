680624bf4f27cd38666ede52a821bde7352278eb
vladpotuk <vladpotuk01@gmail.com>
2024-03-20 20:09:34 +0200

    update

---
+++ ArrayFilters.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ArrayOperations
+{
+    public class ArrayFilters
+    {
+        
+        public static void PrintFilteredArray(int[] array, Func<int, bool> checkMethod)
+        {
+            foreach (int number in array)
+            {
+                if (checkMethod(number))
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+
+        
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        
+        public static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        
+        public static bool IsFibonacci(int number)
+        {
+            return IsPerfectSquare(5 * number * number + 4) ||
+                   IsPerfectSquare(5 * number * number - 4);
+        }
+
+        
+        static bool IsPerfectSquare(int x)
+        {
+            int s = (int)Math.Sqrt(x);
+            return (s * s == x);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ArrayOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Парні числа:");
+            ArrayFilters.PrintFilteredArray(array, ArrayFilters.IsEven);
+
+            Console.WriteLine("\nНепарні числа:");
+            ArrayFilters.PrintFilteredArray(array, ArrayFilters.IsOdd);
+
+            Console.WriteLine("\nПрості числа:");
+            ArrayFilters.PrintFilteredArray(array, ArrayFilters.IsPrime);
+
+            Console.WriteLine("\nЧисла Фібоначчі:");
+            ArrayFilters.PrintFilteredArray(array, ArrayFilters.IsFibonacci);
+        }
+    }
+}
