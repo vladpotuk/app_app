e9f85110fd9745f87a12e85d8578cb6ff116d624
vladpotuk <vladpotuk01@gmail.com>
2024-03-20 18:47:22 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+delegate void DisplayMessage(string message);
+
+class Program
+{
+    static void ShowMessage(string message)
+    {
+        Console.WriteLine($"Повідомлення: {message}");
+    }
+
+    static void ShowMessageInTitle(string message)
+    {
+        Console.Title = message;
+    }
+
+    static void Main(string[] args)
+    {
+        
+        DisplayMessage display = ShowMessage;
+
+        
+        display("Hello,Vlad!");
+
+       
+        display = ShowMessageInTitle;
+
+       
+        display("Новий заголовок ");
+    }
+}
